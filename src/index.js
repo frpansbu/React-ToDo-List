9b47b0bc3dfc82e50f174dff56c51a6bc0720ce6
frpansbu <frpan@cs.stonybrook.edu>
2019-10-08 18:31:00 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/todo_layout.css';
+import './css/todo_style.css';
+import App from './App';
+import './css/fonts/Lexend Exa/css.css'
+
+ReactDOM.render(<App />, document.getElementById('root'));
