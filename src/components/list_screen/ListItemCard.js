b9e85d769c9f54a86c352e7c1c5822fcd8cb755a
frpansbu <frpan@cs.stonybrook.edu>
2019-10-09 19:49:04 -0400

    added completed/pending

--- src/components/list_screen/ListItemCard.js
+++ src/components/list_screen/ListItemCard.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react'
 
 export class ListItemCard extends Component {
+    getStyle = () =>{
+        return{
+            display: this.props.listItem.completed ? "inline-block":"none",
+        }
+    }
+    getStyle2 = () =>{
+        return{
+            display: this.props.listItem.completed ? "none":"inline-block",
+        }
+    }
     render() {
         return (
             <div className='list_item_card'>
@@ -14,8 +24,11 @@ export class ListItemCard extends Component {
                 <div className='list_item_card_due_date'>
                     {this.props.listItem.due_date}
                 </div>
-                <div className='list_item_card_completed'>
-                    {this.props.listItem.completed}
+                <div className = "list_item_card_completed" 
+                style = {this.getStyle()}>Completed
+                </div>
+                <div className = "list_item_card_not_completed"
+                style = {this.getStyle2()}>Pending
                 </div>
                 <div className = "list_item_card_toolbar">
                     <span className = "list_item_card_button">&#x21e7;</span>
